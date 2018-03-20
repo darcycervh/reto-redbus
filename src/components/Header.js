120904b6f6d51e3187d74e7987a819ce6706aebf
aurorex <selva_beat@hotmail.com>
2018-03-19 20:32:08 -0500

    agregando dos nuevos componentes con contenido de prueba

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import propTypes from 'prop-types';
+
+class Header extends Component {
+    render(){
+        return(
+            <header>
+                hola mundo
+            </header>
+        )
+    }
+}
+
+export default Header;
---
+++ src/components/Pago_efectivo.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+// import Header from './Header';
+
+class PagoEfectivo extends Component {
+    render(){
+        return(
+          <header>hola otra vez</header> 
+        )
+    }
+
+}
+
+export default PagoEfectivo;
