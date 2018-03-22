f1b7e0c0e05b78be620f2360e0c55ced6e346fc4
aurorex <selva_beat@hotmail.com>
2018-03-22 07:56:15 -0500

    enlazando cambios 2

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import {render} from 'react-dom';
 import './css/index.css';
-<<<<<<< HEAD
 import App from './components/App';
 
 //ReactDOM.render(<App />, document.getElementById('root'));
@@ -10,15 +9,3 @@ render(
     <App />,
     document.getElementById('root')
 )
-=======
-import App from './components/app';
-
-render(
-    <App />,
-    document.getElementById('root')
-)
-
-//ReactDOM.render(<App />, document.getElementById('root'));
-//registerServiceWorker();
-
->>>>>>> a9ffb7cd7f63b4333b3071c1dd14188bd85fda28
